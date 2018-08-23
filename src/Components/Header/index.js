069dad8a4ecd5dbab2955cd985e7991c3e6cc372
Gilad Lev Ari <GiladL@cet.ac.il>
2018-08-23 09:33:44 +0300

    lowercase header component and css classes

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,9 +4,9 @@ import './style.css';
 
 const Header = () => {
   return (
-    <div className='Header'>
-      <h1 className='Header-title'>Demo Blog</h1>
-      <p className='Header-p'>
+    <div className='header'>
+      <h1 className='header-title'>Demo Blog Posts</h1>
+      <p className='header-p'>
         This app loads 100 fake Posts <br/>
         You can select any of them to get fake comments <br/>
         Yau can also start typing a user name in order to filter posts / comments by user <br/>
