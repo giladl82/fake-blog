8ea90b3c61ac875532f1f39910b370779053c48d
Gilad Lev-Ari <giladl82@gmail.com>
2018-08-25 08:04:34 +0300

    Fixing display

--- src/Components/Filters/index.js
+++ src/Components/Filters/index.js
@@ -7,26 +7,32 @@ import './style.css';
 
 const perPageOptions = [ '5', '10', '20', '50' ]
 
-const Filters = ( { searchBy, onSearchByChange, perPage, onPerPageChange } ) => {
+const Filters = ( { displaying, searchBy, onSearchByChange, perPage, onPerPageChange } ) => {
   return (
     <div className='filters'>
       <Search className='filters-search' value={searchBy} onChange={onSearchByChange} />
-
+      Number of {displaying}&nbsp;
       <Select
         className='filters-select'
         options={perPageOptions}
         onChange={onPerPageChange}
         value={perPage.toString()}
       />
+      &nbsp;per page
     </div>
   )
 }
 
 Filters.propTypes = {
+  displaing: PropTypes.string,
   searchBy: PropTypes.string.isRequired,
   onSearchByChange: PropTypes.func.isRequired,
   perPage: PropTypes.number.isRequired,
   onPerPageChange: PropTypes.func.isRequired
+}
+
+Filters.defaultProps = {
+  displaying: 'posts'
 }
 
 export default Filters;
