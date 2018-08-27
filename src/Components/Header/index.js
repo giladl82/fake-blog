8f345f0ff6f0ce8d96706e2f8f4554d1efd03c38
Gilad Lev-Ari <giladl82@gmail.com>
2018-08-27 10:15:54 +0300

    Fixing merge problems

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,11 +5,7 @@ import './style.css';
 const Header = () => {
   return (
     <div className='header'>
-<<<<<<< HEAD
-      <h1 className='header-title'>Demo Blog Posts</h1>
-=======
       <h1 className='header-title'>Demo Blog</h1>
->>>>>>> 4d52d73a0e878286dd5801dce008068ce5400d1c
       <p className='header-p'>
         This app loads 100 fake Posts <br/>
         You can select any of them to get fake comments <br/>
