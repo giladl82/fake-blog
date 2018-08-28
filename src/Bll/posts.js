f4e2c127d9bfba665921e26a3b691cf3caf0604f
Gilad Lev-Ari <giladl82@gmail.com>
2018-08-28 20:31:47 +0300

    Caching api call for posts

--- src/Bll/posts.js
+++ src/Bll/posts.js
@@ -3,27 +3,41 @@ import { getUsers, findUserById } from './users';
 const filterPostsByText = (posts, filterText) => posts.filter(post => post.title.includes(filterText));
 const filterPostByUserId = (posts, userId) => posts.filter(post => post.userId.toString() === userId || !userId);
 
+let cachedPosts;
+
+const fetchPosts = () =>
+  new Promise(resolve => {
+    if (cachedPosts) {
+      resolve(cachedPosts);
+    } else {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(posts => {
+          cachedPosts = posts;
+          resolve(cachedPosts);
+        });
+    }
+  });
+
 export const getPosts = (currentPageNumber, numberOfPostsPerPage, filterText, userId) => {
   return new Promise(resolve => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(posts => {
-        numberOfPostsPerPage = parseInt(numberOfPostsPerPage, 10);
-        const startIndex = (currentPageNumber - 1) * numberOfPostsPerPage;
-        let filterPosts = filterPostsByText(posts, filterText);
-        
-        filterPosts = filterPostByUserId(filterPosts, userId);
-        filterPosts = filterPosts.slice(startIndex, startIndex + numberOfPostsPerPage);
-        getUsers().then(users => {
-          filterPosts.forEach(post => {
-            const user = findUserById(users, post.userId);
-            if (user) {
-              post.userName = user.name;
-            }
-          });
+    fetchPosts().then(posts => {
+      numberOfPostsPerPage = parseInt(numberOfPostsPerPage, 10);
+      const startIndex = (currentPageNumber - 1) * numberOfPostsPerPage;
+      let filterPosts = filterPostsByText(posts, filterText);
 
-          resolve(filterPosts);
+      filterPosts = filterPostByUserId(filterPosts, userId);
+      filterPosts = filterPosts.slice(startIndex, startIndex + numberOfPostsPerPage);
+      getUsers().then(users => {
+        filterPosts.forEach(post => {
+          const user = findUserById(users, post.userId);
+          if (user) {
+            post.userName = user.name;
+          }
         });
+
+        resolve(filterPosts);
       });
+    });
   });
 };
