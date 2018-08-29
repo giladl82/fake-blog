28aa6ec9eec7e0c500e2e8c0a5877238ac88a6bd
Gilad Lev-Ari <giladl82@gmail.com>
2018-08-29 14:50:25 +0300

    Fixing Header text

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -9,7 +9,6 @@ const Header = () => {
       <p className='header-p'>
         This app loads 100 fake Posts <br/>
         You can select any of them to get fake comments <br/>
-        Yau can also start typing a user name in order to filter posts / comments by user <br/>
         All posts and comments are resulted by <a className='header-p-link' href='jsonplaceholder.typicode.com'>jsonplaceholder.typicode.com</a>
       </p>
     </div>
