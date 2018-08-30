7d83e8e9d2ac75f6b4ee7d5dc958f251fafe039d
Gilad Lev-Ari <giladl82@gmail.com>
2018-08-30 22:23:08 +0300

    Runing prettier

--- src/Components/Post/index.js
+++ src/Components/Post/index.js
@@ -5,20 +5,20 @@ import './style.css';
 
 const Post = ({ data }) => {
   return (
-    <div className='post'>
-      <h2 className='post-title'>{data.title}</h2>
-      {data.userName && <div className='post-creator'>By: {data.userName}</div>}
-      <div className='post-summary'>{data.body}</div>
+    <div className="post">
+      <h2 className="post-title">{data.title}</h2>
+      {data.userName && <div className="post-creator">By: {data.userName}</div>}
+      <div className="post-summary">{data.body}</div>
     </div>
-  )
-}
+  );
+};
 
-Post.propTypes=  {
+Post.propTypes = {
   data: PropTypes.shape({
     userId: PropTypes.number.isRequired,
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired
   }).isRequired
-}
+};
 
 export default Post;
